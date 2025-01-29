63ac27f17bb668d5acdb34c18bec0470dc414082
IvSokolaN <sokolov.wd@bk.ru>
2025-01-29 16:34:33 +0800

    feat(user): в таблицу users добавлена колонка баланса денег; добавлена модель Баллы с миграцией для отслеживания истории начисления или списания баллов

---
+++ app/Models/Balance.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Balance extends Model
+{
+    protected $fillable = [
+        'balance',
+        'user_id',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Models/Point.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Point extends Model
+{
+    protected $fillable = [
+        'point',
+        'reason',
+        'in',
+        'out',
+        'user_id',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
